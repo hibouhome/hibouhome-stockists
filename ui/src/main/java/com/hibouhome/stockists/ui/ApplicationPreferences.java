ab1da8243efc626b94c723e8eb137e3b3cc646da
Jonathan Wright <hibouhome@users.noreply.github.com>
2014-06-27 20:39:02 +0100

    Added checks to cater for invalid preference values

--- ui/src/main/java/com/hibouhome/stockists/ui/ApplicationPreferences.java
+++ ui/src/main/java/com/hibouhome/stockists/ui/ApplicationPreferences.java
@@ -18,16 +18,37 @@ public class ApplicationPreferences {
 		preferences = Preferences.userNodeForPackage(ApplicationPreferences.class);
 	}
 
+	/**
+	 * Gets the directory for the open file dialog
+	 * 
+	 * @return the directory containing the last file opened by the application. If the application
+	 *         hasn't yet opened a file, or the directory referenced by this preference no longer
+	 *         exists, the user's home directory will be returned
+	 */
 	public File getLastDir() {
 		final String path = preferences.get(LAST_DIR, System.getProperty("user.home"));
-		return new File(path);
+		final File dir = new File(path);
+		// check the directory still exists as the user may have moved or deleted it since last
+		// time the application was run
+		if (dir.isDirectory()) {
+			return dir;
+		} else {
+			// last directory preference has been corrupted, return the user's home directory
+			return new File(System.getProperty("user.home"));
+		}
 	}
 
+	/**
+	 * Sets the value of the directory containing the most recently opened file
+	 * 
+	 * @param file the {@link File}. If this value is null or not a valid directory the preference
+	 *            will be set to null
+	 */
 	public void setLastDir(final File file) {
-		if (file.isDirectory()) {
+		if (file != null && file.isDirectory()) {
 			preferences.put(LAST_DIR, file.getAbsolutePath());
 		} else {
-			throw new IllegalArgumentException(file.getAbsolutePath() + " is not a valid directory");
+			preferences.put(LAST_DIR, null);
 		}
 	}
 
